97a9389b9031a6327668150663e6ad8d8bffac64
clausvalca226 <50083224+clausvalca226@users.noreply.github.com>
2023-05-04 12:51:49 -0400

    Update shim.d.ts

--- shim.d.ts
+++ shim.d.ts
@@ -6,12 +6,16 @@ declare namespace flashstorage {
 	//% parts="flashstorage"
 	//% blockGap=8
 	//% group="micro:bit (V2)" shim=flashlog::storeData
-    function storeData(key: string, value: string): int32;
+    function storeData(key: string, value: string): number {
+	    return 0;
+    }
 
     /* Retrieves the given key/value pair. */
 	//% help=flashstorage/get-data
 	//% parts="flashstorage"
 	//% blockGap=8
 	//% group="micro:bit (V2)" shim=flashlog::getData
-    function getData(key: string): string;
+    function getData(key: string): string {
+	    return "";
+    }
 }
