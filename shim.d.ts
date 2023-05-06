1ed5ebee938e64ce55d8b092027cc5d2e56440d6
clausvalca226 <50083224+clausvalca226@users.noreply.github.com>
2023-05-05 23:46:38 -0400

    Update shim.d.ts

--- shim.d.ts
+++ shim.d.ts
@@ -1,17 +1,5 @@
 /* Storing structured data in flash. */
-//%
-declare namespace flashstorage {
-    /* Stores the given key/value pair. */
-	//% help=flashstorage/store-data
-	//% parts="flashstorage"
-	//% blockGap=8
-	//% group="micro:bit (V2)" shim=flashstorage::storeData
-    function storeData(key: string, value: string): number;
 
-    /* Retrieves the given key/value pair. */
-	//% help=flashstorage/get-data
-	//% parts="flashstorage"
-	//% blockGap=8
-	//% group="micro:bit (V2)" shim=flashstorage::getData
-    function getData(key: string): string;
+declare namespace flashstorage {
+
 }
