6fc9348e0b77ba573abfd8d3227d12bd0f7e68e9
clausvalca226 <50083224+clausvalca226@users.noreply.github.com>
2023-05-06 20:34:17 -0400

    Update flashstorage.ts

--- flashstorage.ts
+++ flashstorage.ts
@@ -1,9 +1,5 @@
-//% color=#0000FF 
-//% icon="\uf074"
-//% block="i2c Reroute"
-//% group="micro:bit (V2)"
 namespace flashstorage {
-    //% blockId="storeData" block="later"
+    //% blockId="flashstoragestoreData" block="later"
     //% shim=flashstorage::storeData
     //% key.defl="test value.defl="test"
     //% group="micro:bit (V2)"
@@ -11,7 +7,7 @@ namespace flashstorage {
         // Per https://github.com/microsoft/pxt-microbit/issues/4292
         return 0;
     }
-    //% blockId="getData" block="later"
+    //% blockId="flashstoragegetData" block="later"
     //% shim=flashstorage::getData
     //% key.defl="test"
     //% group="micro:bit (V2)"
